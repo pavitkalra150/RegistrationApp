e3a8ec3ddecad289952c55bcd90ba7a515318bdf
pavitkalra150 <c0868150@mylambton.ca>
2022-11-26 20:34:14 -0500

    Initial setup for a Registration Application on Android

---
+++ app/src/main/java/com/example/registrationapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.registrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        checkData();
+    }
+
+    public void checkData(){
+
+    }
+
+}
